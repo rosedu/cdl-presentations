ea21eb948be6f53eb3cd0cf4fa97f35926c83c97
Andrei Soare <andrei.soare@gmail.com>
2009-03-07 09:51:19 +0200

    Finalizat slideshow #:-s

--- curs1/andrei.soare/highlight/06.sh
+++
@@ -1,8 +0,0 @@
-andrei@andrei-vm:~/work$ ls
-hello.c
-andrei@andrei-vm:~/work$ make hello
-cc     hello.c   -o hello
-andrei@andrei-vm:~/work$ ls
-hello  hello.c
-andrei@andrei-vm:~/work$ ./hello
-Hello !
