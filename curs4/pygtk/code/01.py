a570534a465127d73a7413dd8544d8a2423b9295
Mihai Maruseac <mihai.maruseac@gmail.com>
2009-03-27 23:31:11 +0200

    Completat curs 4

---
+++ curs4/pygtk/code/01.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+import pygtk
+pygtk.require('2.0')
+import gtk
+
+class Hello:
+    def __init__(self):
+        self.w = gtk.window(gtk.WINDOW_TOPLEVEL)
+	self.w.show()
+
+hello = Hello()
