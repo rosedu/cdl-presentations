9ce1e501032dfe47d120d61adc659c7d4320a4ec
Lucian Adrian Grijincu <lucian.grijincu@gmail.com>
2009-03-28 10:28:23 +0200

    pythonexecriții

---
+++ curs4/python/exercitii/1_tuple-list.py
@@ -0,0 +1,7 @@
+#!/usr/binenv python
+#Purchase Date |  Purchase Price  | Shares |  Symbol | Current Price
+portfolio= [ ( "25-Jan-2001", 43.50, 25, 'CAT', 92.45 ),
+             ( "25-Jan-2001", 42.80, 50, 'DD', 51.19 ),
+             ( "25-Jan-2001", 42.10, 75, 'EK', 34.87 ),
+             ( "25-Jan-2001", 37.58, 100, 'GM', 37.58 ) ]
+
