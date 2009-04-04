493d94d0115278ff7686769ecdce9a5a935c6bdd
Lucian Adrian Grijincu <lucian.grijincu@gmail.com>
2009-04-04 07:01:42 +0300

    deb: expl for deb files

--- curs5/deb/code/1_wget.sh
+++ curs5/deb/code/1_wget.sh
@@ -4,3 +4,5 @@ cd ~/hello
 wget http://ftp.gnu.org/gnu/hello/hello-2.1.1.tar.gz
 cp hello-2.1.1.tar.gz hello_2.1.1.orig.tar.gz
 tar -xzvf hello_2.1.1.orig.tar.gz
+cd hello-2.1.1
+dh_make -e lucian.grijincu@rosedu.org
---
+++ curs5/deb/code/2_rm_unneeded.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+cd ~/hello/debian/
+rm *.ex *.EX dirs docs info README.Debian
