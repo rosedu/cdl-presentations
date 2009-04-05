4ec8b4a69b0ea651f4a61bebb450ee0c9a62d1a3
Lucian Adrian Grijincu <lucian.grijincu@gmail.com>
2009-04-05 13:22:06 +0300

    deb: typo fix, add DEBFULLNAME to code/1_wget.sh, center "?" at end

--- curs5/deb/code/1_wget.sh
+++ curs5/deb/code/1_wget.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+# trebuie să aveți o cheie gpg cu această adresă și acest nume
+export DEBEMAIL="lucian.grijincu@rosedu.org"
+export DEBFULLNAME="Lucian Adrian Grijincu"
 mkdir ~/hello
 cd ~/hello
 wget http://ftp.gnu.org/gnu/hello/hello-2.1.1.tar.gz
