32c0f9e1cbbe0f0033a49f89ecade1e26375db1a
Mihai Maruseac <mihai.maruseac@gmail.com>
2010-04-24 07:39:00 +0300

    Standardizat curs 6.

--- 2010/curs3/django/highlight/model.py
+++
@@ -1,8 +0,0 @@
-class Artifact(models.Model):
-    """ Artifact object, now streamline """
-    name = models.CharField(max_length=100)
-    description = models.TextField(blank=True)
-    image = models.ImageField(upload_to=config.MEDIA_ARTIFACTS)
-    
-    def __unicode__(self):
-        return self.name
