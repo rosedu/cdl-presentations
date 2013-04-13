0a866c618953f05f6a32f6c6b71b5743045981cc
Mihai Maruseac <mihai@rosedu.org>
2013-04-13 18:32:21 +0300

    2013-spring-cleanup: Beamer rule cleanup.

--- latex-beamer.mk
+++ latex-beamer.mk
@@ -1,5 +1,4 @@
 PDF = $(addsuffix .pdf, $(BASENAME))
-TEX = $(addsuffix .tex, $(BASENAME))
 PDFLATEX = pdflatex
 OUT_DIR = texfiles
 STY_DIR = $(DEPTH)/template/
@@ -8,12 +7,12 @@ STY_DIR = $(DEPTH)/template/
 
 all: $(PDF)
 
-$(PDF): $(TEX)
+%.pdf: %.tex
 	@test -d $(OUT_DIR) || mkdir $(OUT_DIR)
 	# Twice, so TOC is also updated
 	@TEXINPUTS=$(STY_DIR)//: $(PDFLATEX) -output-directory $(OUT_DIR) $<
 	@TEXINPUTS=$(STY_DIR)//: $(PDFLATEX) -output-directory $(OUT_DIR) $<
-	@ln -sf $(OUT_DIR)/$(PDF) .
+	@ln -sf $(OUT_DIR)/$@ .
 
 clean:
 	@$(RM) -r $(OUT_DIR)
