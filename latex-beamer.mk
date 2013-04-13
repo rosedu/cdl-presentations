68ac20c1f9938f0c5fe1f9d681ec045b9a81b715
Mihai Maruseac <mihai@rosedu.org>
2013-04-13 20:01:28 +0300

    2013-spring-cleanup: Add highlight rules to latex-beamer.mk

--- latex-beamer.mk
+++ latex-beamer.mk
@@ -1,19 +1,36 @@
 PDF = $(addsuffix .pdf, $(BASENAME))
+TEX = $(addsuffix .tex, $(BASENAME))
+
 PDFLATEX = pdflatex
 OUT_DIR = texfiles
 STY_DIR = $(DEPTH)/template/
+
+CODEINPUTSTEX = $(addprefix $(CODEDIR)/, $(addsuffix .tex, $(CODEINPUTS)))
+
+HIGHLIGHTSTY = highlight.sty
+
+ifndef $(TABSIZE)
+	TABSIZE = 8
+endif
+
+HIGHLIGHT_OPTS = --out-format=latex -l -f -t $(TABSIZE) -c $(HIGHLIGHTSTY)
 
 .PHONY: clean all
 
 all: $(PDF)
 
-%.pdf: %.tex
+$(PDF): $(TEX) $(CODEINPUTSTEX)
+	@# Construct $(OUT_DIR) if it doesn't exist
 	@test -d $(OUT_DIR) || mkdir $(OUT_DIR)
-	# Twice, so TOC is also updated
+	@# Twice, so TOC is also updated
 	@TEXINPUTS=$(STY_DIR)//: $(PDFLATEX) -output-directory $(OUT_DIR) $<
 	@TEXINPUTS=$(STY_DIR)//: $(PDFLATEX) -output-directory $(OUT_DIR) $<
 	@ln -sf $(OUT_DIR)/$@ .
 
 clean:
 	@$(RM) -r $(OUT_DIR)
-	@$(RM) $(PDF)
+	@$(RM) $(PDF) $(CODEINPUTSTEX) $(CODEDIR)/$(HIGHLIGHTSTY)
+
+# highlight rules
+%.cpp.tex: %.cpp
+	@highlight $(HIGHLIGHT_OPTS) -i $< -o $@
