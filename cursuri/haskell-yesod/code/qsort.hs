60dfd35e67a0c723fe22d3abe400dcbf9baf1673
Mihai Maruseac <mihai@rosedu.org>
2013-04-15 19:33:56 +0300

    yesod: Fix qsort example in Haskell.

--- cursuri/haskell-yesod/code/qsort.hs
+++ cursuri/haskell-yesod/code/qsort.hs
@@ -1,5 +1,5 @@
 qsort []     = []
-qsort (x:xs) = lesser ++ greater
+qsort (x:xs) = lesser ++ (x : greater)
   where
     lesser = qsort [y | y <- xs, y < x]
     greater = qsort [y | y <- xs, y >= x]
